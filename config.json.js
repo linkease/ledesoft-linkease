9daf9c79b642d195fed39665619c06be6908930a
janson <janson@gmail.com>
2020-09-09 11:53:52 +0800

    v0.2.99

--- config.json.js
+++ config.json.js
@@ -1,10 +1,10 @@
 {
 "module":"linkease",
 "version":"2.1.1",
-"md5":"4145492dc8a12f8b2f1ffd8b29dd3422",
+"md5":"1df5d5e4cc45eb78aae0876bfc70c90b",
 "home_url":"Module_linkease.asp",
 "title":"易有云2.0 - LinkEase",
 "description":"多用户全平台文件管理工具，支持双向同步！",
 "changelog":"更新全新 2.0 版本",
-"build_date":"2020-09-05_00:25:03"
+"build_date":"2020-09-09_11:53:42"
 }
