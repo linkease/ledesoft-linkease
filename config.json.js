44053165d2b4b8f0e45ece4e37ade88045f1f3fb
janson <janson@gmail.com>
2020-11-26 18:49:20 +0800

    upgrade new binaries

--- config.json.js
+++ config.json.js
@@ -1,10 +1,10 @@
 {
 "module":"linkease",
 "version":"2.1.1",
-"md5":"1df5d5e4cc45eb78aae0876bfc70c90b",
+"md5":"ec860923ac89ef1dd43d424762924c2d",
 "home_url":"Module_linkease.asp",
 "title":"易有云2.0 - LinkEase",
 "description":"多用户全平台文件管理工具，支持双向同步！",
 "changelog":"更新全新 2.0 版本",
-"build_date":"2020-09-09_11:53:42"
+"build_date":"2020-11-26_16:55:44"
 }
