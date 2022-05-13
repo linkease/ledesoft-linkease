ed2247a40a70aaf2edb36ee8f5286109341b0741
janson <janson@gmail.com>
2022-05-14 05:09:06 +0800

    bump to new version

--- config.json.js
+++ config.json.js
@@ -1,10 +1,10 @@
 {
 "module":"linkease",
-"version":"2.1.2",
+"version":"2.7.6",
 "md5":"c9b06f0f412e3e2569d61e9586949a28",
 "home_url":"Module_linkease.asp",
 "title":"易有云2.0 - LinkEase",
-"description":"多用户全平台文件管理工具，支持双向同步！",
+"description":"多用户全平台文件管理工具，支持相册备份，远程下载，文件同步！",
 "changelog":"更新全新 2.0 版本",
 "build_date":"2021-04-23_17:27:32"
 }
